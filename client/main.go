ea9bc5d863a42f22ccbc954e158f492ec93d0800
vorty <uf4vortex@gmail.com>
2025-02-13 20:00:21 +0100

    near to finished client

--- client/main.go
+++ client/main.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
   "net"
   "os"
+  "time"
+  "strings"
 
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
 	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/lipgloss"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+//TODO close current connection + align properly
 var (
-	keywordStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("204")).Background(lipgloss.Color("235"))
+  keywordStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("204")).Background(lipgloss.Color("235"))
 	helpStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
+  foregroundStyle = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("5"))
+  spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("5"))
+  errStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("9"))
 )
 
 /* client connection to the tcp server */
@@ -29,6 +37,8 @@ const (
 type (
 	errMsg error
 	connectionMsg struct{} 
+  sendMsg struct{}
+  recvMsg struct{msg string}
 )
 
 /* model */
@@ -36,11 +46,20 @@ type model struct {
 	state 	  int 
 	spinner   spinner.Model
 	textInput textinput.Model
+  messages  []string
+  viewport  viewport.Model
+  textArea  textarea.Model
 	err       error
 }
 
-func main() {
-	p := tea.NewProgram(initialModel())
+func main() { 
+  defer func() { /* if a connection has been establish close it */ 
+    if conn != nil {
+      conn.Close()
+    }
+  } ()
+
+  p := tea.NewProgram(initialModel()) /* start bubbletea program */
 	if _, err := p.Run(); err != nil {
     fmt.Printf("Got an error : %v\n", err);
     os.Exit(1)
@@ -48,19 +67,42 @@ func main() {
 }
 
 func initialModel() model {
+  /* NotConnected view */
 	ti := textinput.New()
 	ti.Placeholder = "localhost:8080"
 	ti.Focus()
 	ti.CharLimit = 156
 	ti.Width = 20
 
+  /* Connecting view */
 	sp := spinner.New()
-	sp.Spinner = spinner.Line
+	sp.Spinner = spinner.Points
+  sp.Style = spinnerStyle
+
+  /* Connected view */
+  ta := textarea.New()
+  ta.Placeholder = "Send a message..."
+  ta.Focus()
+
+  ta.Prompt = "┃ "
+  ta.CharLimit = 280
+  
+  ta.SetWidth(50)
+  ta.SetHeight(3)
+  ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+  ta.ShowLineNumbers = false
+  ta.KeyMap.InsertNewline.SetEnabled(false)
+
+  vp := viewport.New(50, 5)
+  vp.SetContent("Welcome from server")
 
 	return model{
 		state:      NotConnected,
+    messages:   []string{},
 		spinner:    sp,
 		textInput:  ti,
+    viewport:   vp,
+    textArea:   ta,
 		err:        nil,
 	}
 }
@@ -73,8 +115,33 @@ func Connect(m model) tea.Cmd {
     if err != nil {
       return errMsg(err)
     }
+    
+    //Wait just to see the beautifull spinner :)
+    time.Sleep(2 * time.Second)
+
 		return connectionMsg{} 
 	}
+}
+
+func Send(m model, send string) tea.Cmd {
+  return func() tea.Msg {
+    //Send the messages
+    _, err := conn.Write([]byte(send))
+    if err != nil {
+      return errMsg(err)
+    }
+    
+    //Buffer for read
+    reply := make([]byte, 280) //TODO check BUFSIZE ??? i think 8192
+    //Wait and read the server reply
+    var n int
+    n, err = conn.Read(reply)
+    if err != nil {
+      return errMsg(err)
+    }
+
+    return recvMsg{msg: foregroundStyle.Render("server: ") + string(reply[0:n])}  
+  }
 }
 
 func (m model) Init() tea.Cmd {
@@ -82,58 +149,142 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC, tea.KeyEsc:
-			return m, tea.Quit
-		case tea.KeyEnter:
-      if(m.state == NotConnected){
-			  return m, Connect(m) 
-      }
-		}
-
-  // Connection succeed
-  case connectionMsg:
-    m.state++
-    return m, m.spinner.Tick  
-
-	// We handle errors just like any other message
-	case errMsg:
-		m.err = msg
-		return m, tea.Quit 
-	}
-
-	if(m.state == NotConnected){
-		m.textInput, cmd = m.textInput.Update(msg)
-	}
-
-	if(m.state == Connecting){
-		m.spinner, cmd = m.spinner.Update(msg)
-	}
-	return m, cmd
-}
-
-func (m model) View() string{
-  if m.err != nil {
-    return fmt.Sprintf("\n\nerror -> %v\n\n", m.err) + "\n"
+  /* this key always quit */
+  if msg, ok := msg.(tea.KeyMsg); ok {
+    k := msg.String()
+    if k == "esc" || k == "ctrl+c" {
+      return m, tea.Quit
+    }
   }
 
+  if(m.state == Connecting){
+    return updateConnecting(msg, m)
+  }
+
+  if(m.state == Connected){
+    return updateConnected(msg, m)
+  }
+
+  return updateNotConnected(msg, m)
+}
+
+func updateNotConnected(msg tea.Msg, m model) (tea.Model, tea.Cmd){
+  switch msg := msg.(type) {
+    case tea.KeyMsg:
+    switch msg.Type {
+      case tea.KeyEnter:
+        m.state++
+        return m, tea.Batch(Connect(m), m.spinner.Tick) 
+    }
+  }
+
+  var cmd tea.Cmd
+  m.textInput, cmd = m.textInput.Update(msg)
+
+  return m, cmd
+}
+
+func updateConnecting(msg tea.Msg, m model) (tea.Model, tea.Cmd){
+  switch msg := msg.(type) {
+    case connectionMsg:
+      m.state++
+      return m, tea.Batch(textarea.Blink, tea.WindowSize())  
+
+    // We handle errors just like any other message
+    case errMsg:
+      m.state--
+      m.err = msg
+      return m, nil  
+  }
+
+  var cmd tea.Cmd
+  m.spinner, cmd = m.spinner.Update(msg)
+
+  return m, cmd
+}
+
+func updateConnected(msg tea.Msg, m model) (tea.Model, tea.Cmd){
+  var (
+    tiCmd tea.Cmd
+    vpCmd tea.Cmd
+  )
+
+  m.textArea, tiCmd = m.textArea.Update(msg)
+  m.viewport, vpCmd = m.viewport.Update(msg)
+
+  switch msg := msg.(type) {
+  case tea.WindowSizeMsg:
+    m.viewport.Width = msg.Width 
+    m.textArea.SetWidth(msg.Width)
+    m.viewport.Height = msg.Height - m.textArea.Height() - lipgloss.Height("\n\n\n\n") //gap + helpline + connectedline
+    if len(m.messages) > 0 {
+      m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+    }
+    m.viewport.GotoBottom()
+
+  case tea.KeyMsg:
+    switch msg.Type {
+    case tea.KeyEnter:
+      str := m.textArea.Value()
+      m.messages = append(m.messages, foregroundStyle.Render("you: ") + str) 
+      m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+      m.textArea.Reset()
+      m.viewport.GotoBottom()
+      return m, Send(m, str) 
+    }
+
+  case recvMsg:
+    m.messages = append(m.messages, msg.msg) 
+    m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+    m.viewport.GotoBottom()
+  }
+
+  return m, tea.Batch(tiCmd, vpCmd) 
+}
+
+/* Main view */
+func (m model) View() string{
 	if(m.state == Connecting){
 		return ConnectingView(m)
 	}
+
+  if(m.state == Connected){
+    return ConnectedView(m)
+  }
 	return NotConnectedView(m)
 }
 
 func NotConnectedView(m model) string {
-	return fmt.Sprintf("What's host ip address port?\n\n%s\n\n%s",
-					m.textInput.View(),
-					"press esc to quit") + "\n"
+  var str string
+  str += "What's host "
+  str += foregroundStyle.Render("ip:port") 
+  str += " ? " 
+
+  if m.err != nil {
+    str += errStyle.Render(fmt.Sprintf("%v", m.err))
+  }
+
+  str += "\n\n"
+  str += m.textInput.View()
+  str += "\n\n"
+
+  str += helpStyle.Render(" 󰌑 : to start connection • ctrl+c, q: exit")
+  
+  return str + "\n"
 }
 
 func ConnectingView(m model) string {
-  conn.Write([]byte("FuckOff"))
-  return fmt.Sprintf("\n\n %s wait till connect to \n\n", m.spinner.View()) + "\n"
+  return fmt.Sprintf("\n\n %s wait till connect to %s\n\n", 
+          m.spinner.View(),
+          foregroundStyle.Render(m.textInput.Value())) + "\n"
+}
+
+func ConnectedView(m model) string {
+  var str string
+  str += fmt.Sprintf("Connected on %s\n", conn.RemoteAddr().String())
+  str += m.viewport.View()
+  str += "\n\n"
+  str += m.textArea.View()
+  str += "\n\n" + helpStyle.Render(" 󰌑 : send a msg •  , : move along viewport • ctrl+c, q: exit")
+  return str
 }
